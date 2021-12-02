9fecb4429085851ae61d156b8f03b132bed86ec0
조재우 <chojw0727@gmail.com>
2021-12-02 09:05:41 +0000

    Update app.py, models.py, db_connect.py

--- models.py
+++ models.py
@@ -1,4 +1,124 @@
-from db_connect import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+class Metro(db.Model):
+    __tablename__ = 'metro'
+    id = db.Column(db.Integer, primary_key=True)
+    date_day = db.Column(db.DateTime, nullable=False)
+    get_on = db.Column(db.Integer, nullable=False)
+    get_off = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, date_day, get_on , get_off):
+        self.date_day = date_day
+        self.get_on = get_on
+        self.get_off = get_off
+
+
+class Movie(db.Model):
+
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key= True, nullable= False)
+    date_day = db.Column(db.DateTime, nullable= False)
+    korea_sales = db.Column(db.Integer, nullable= False)
+    korea_audience_num = db.Column(db.Integer, nullable=False)
+    foreign_audience_num = db.Column(db.Integer, nullable=False)
+    foreign_sales = db.Column(db.Integer, nullable=False)
+    total_sales = db.Column(db.Integer, nullable=False)
+    total_audience = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, date_day, korea_sales, korea_audience_num, foreign_audience_num,foreign_sales,
+                 total_sales, total_audience):
+        self.date_day = date_day
+        self.korea_sales = korea_sales
+        self.korea_audience_num = korea_audience_num
+        self.foreign_audience_num = foreign_audience_num
+        self.foreign_sales = foreign_sales
+        self.total_sales = total_sales
+        self.total_audience = total_audience
+
+class Consumption(db.Model):
+    __tablename__ = 'consumption'
+    ID = db.Column(db.Integer, primary_key = True)
+    DATE_MON = db.Column(db.String(255), nullable = False)
+    MARKET_NONFOOD_HOMEWARE_CULTURE = db.Column(db.Float(8), nullable = False)
+    MARKET_NONFOOD_CLOTHING = db.Column(db.Float(8), nullable = False)
+    MARKET_NONFOOD_DAILY = db.Column(db.Float(8), nullable = False)
+    MARKET_NONFOOD_SPORT = db.Column(db.Float(8), nullable = False)
+    MARKET_NONFOOD_CHANDLERY = db.Column(db.Float(8), nullable = False)
+    MARKET_NONFOOD_SUBTOTAL = db.Column(db.Float(8), nullable = False)
+    MARKET_FOOD = db.Column(db.Float(8), nullable = False)
+    MARKET_TOTAL = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_CHANDLERY = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_WOMENSUITS = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_WOMENCASUAL = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_MENCLOTHING = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_KIDS_SPORT = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_HOMEWARE = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_LUXURY = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_NONFOOD_SUBTOTAL = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_FOOD = db.Column(db.Float(8), nullable = False)
+    DEPARTMENT_TOTAL = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_NONFOOD_DAILY = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_NONFOOD_CHANDLERY = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_NONFOOD_CIGARETTE_ETC = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_NONFOOD_SUBTOTAL = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_FOOD_BEVERAGE_PROCESSED = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_FOOD_INSTANT_FRESH = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_FOOD_SUBTOTAL = db.Column(db.Float(8), nullable = False)
+    CONVENIENCE_TOTAL = db.Column(db.Float(8), nullable = False)
+    SSM_NONFOOD_DAILY = db.Column(db.Float(8), nullable = False)
+    SSM_NONFOOD_CHANDLERY = db.Column(db.Float(8), nullable = False)
+    SSM_NONFOOD_SUBTOTAL = db.Column(db.Float(8), nullable = False)
+    SSM_FOOD_AGRICULTURAL_MARINE_LIVESTOCK = db.Column(db.Float(8), nullable = False)
+    SSM_FOOD_FRESH = db.Column(db.Float(8), nullable = False)
+    SSM_FOOD_PROCESSED = db.Column(db.Float(8), nullable = False)
+    SSM_FOOD_SUBTOTAL = db.Column(db.Float(8), nullable = False)
+    SSM_TOTAL = db.Column(db.Float(8), nullable = False)
+
+def __init__(self, DATE_MON, MARKET_NONFOOD_HOMEWARE_CULTURE, MARKET_NONFOOD_CLOTHING, MARKET_NONFOOD_DAILY, MARKET_NONFOOD_SPORT, MARKET_NONFOOD_CHANDLERY, MARKET_NONFOOD_SUBTOTAL,
+                MARKET_FOOD, MARKET_TOTAL, DEPARTMENT_NONFOOD_CHANDLERY, DEPARTMENT_NONFOOD_WOMENSUITS, DEPARTMENT_NONFOOD_WOMENCASUAL, DEPARTMENT_NONFOOD_MENCLOTHING, DEPARTMENT_NONFOOD_KIDS_SPORT,
+                DEPARTMENT_NONFOOD_HOMEWARE, DEPARTMENT_NONFOOD_LUXURY, DEPARTMENT_NONFOOD_SUBTOTAL, DEPARTMENT_FOOD, DEPARTMENT_TOTAL, CONVENIENCE_NONFOOD_DAILY, CONVENIENCE_NONFOOD_CHANDLERY,
+                CONVENIENCE_NONFOOD_CIGARETTE_ETC,CONVENIENCE_NONFOOD_SUBTOTAL, CONVENIENCE_FOOD_BEVERAGE_PROCESSED, CONVENIENCE_FOOD_INSTANT_FRESH, CONVENIENCE_FOOD_SUBTOTAL, CONVENIENCE_TOTAL,
+                SSM_NONFOOD_DAILY, SSM_NONFOOD_CHANDLERY, SSM_NONFOOD_SUBTOTAL, SSM_FOOD_AGRICULTURAL_MARINE_LIVESTOCK, SSM_FOOD_FRESH, SSM_FOOD_PROCESSED, SSM_FOOD_SUBTOTAL, SSM_TOTAL):
+        self.DATE_MON = DATE_MON
+        self.MARKET_NONFOOD_HOMEWARE_CULTURE = MARKET_NONFOOD_HOMEWARE_CULTURE
+        self.MARKET_NONFOOD_CLOTHING = MARKET_NONFOOD_CLOTHING
+        self.MARKET_NONFOOD_DAILY = MARKET_NONFOOD_DAILY
+        self.MARKET_NONFOOD_SPORT = MARKET_NONFOOD_SPORT
+        self.MARKET_NONFOOD_CHANDLERY = MARKET_NONFOOD_CHANDLERY
+        self.MARKET_NONFOOD_SUBTOTAL = MARKET_NONFOOD_SUBTOTAL
+        self.MARKET_FOOD = MARKET_FOOD
+        self.MARKET_TOTAL =MARKET_TOTAL
+        self.DEPARTMENT_NONFOOD_CHANDLERY = DEPARTMENT_NONFOOD_CHANDLERY
+        self.DEPARTMENT_NONFOOD_WOMENSUITS = DEPARTMENT_NONFOOD_WOMENSUITS
+        self.DEPARTMENT_NONFOOD_WOMENCASUAL = DEPARTMENT_NONFOOD_WOMENCASUAL
+        self.DEPARTMENT_NONFOOD_MENCLOTHING = DEPARTMENT_NONFOOD_MENCLOTHING
+        self.DEPARTMENT_NONFOOD_KIDS_SPORT = DEPARTMENT_NONFOOD_KIDS_SPORT
+        self.DEPARTMENT_NONFOOD_HOMEWARE = DEPARTMENT_NONFOOD_HOMEWARE
+        self.DEPARTMENT_NONFOOD_LUXURY = DEPARTMENT_NONFOOD_LUXURY
+        self.DEPARTMENT_NONFOOD_SUBTOTAL = DEPARTMENT_NONFOOD_SUBTOTAL
+        self.DEPARTMENT_FOOD = DEPARTMENT_FOOD
+        self.DEPARTMENT_TOTAL = DEPARTMENT_TOTAL
+        self.CONVENIENCE_NONFOOD_DAILY = CONVENIENCE_NONFOOD_DAILY
+        self.CONVENIENCE_NONFOOD_CHANDLERY = CONVENIENCE_NONFOOD_CHANDLERY
+        self.CONVENIENCE_NONFOOD_CIGARETTE_ETC = CONVENIENCE_NONFOOD_CIGARETTE_ETC
+        self.CONVENIENCE_NONFOOD_SUBTOTAL = CONVENIENCE_NONFOOD_SUBTOTAL
+        self.CONVENIENCE_FOOD_BEVERAGE_PROCESSED = CONVENIENCE_FOOD_BEVERAGE_PROCESSED
+        self.CONVENIENCE_FOOD_INSTANT_FRESH = CONVENIENCE_FOOD_INSTANT_FRESH
+        self.CONVENIENCE_FOOD_SUBTOTAL = CONVENIENCE_FOOD_SUBTOTAL
+        self.CONVENIENCE_TOTAL = CONVENIENCE_TOTAL
+        self.SSM_NONFOOD_DAILY = SSM_NONFOOD_DAILY
+        self.SSM_NONFOOD_CHANDLERY = SSM_NONFOOD_CHANDLERY
+        self.SSM_NONFOOD_SUBTOTAL = SSM_NONFOOD_SUBTOTAL
+        self.SSM_FOOD_AGRICULTURAL_MARINE_LIVESTOCK = SSM_FOOD_AGRICULTURAL_MARINE_LIVESTOCK
+        self.SSM_FOOD_FRESH = SSM_FOOD_FRESH
+        self.SSM_FOOD_PROCESSED = SSM_FOOD_PROCESSED
+        self.SSM_FOOD_SUBTOTAL = SSM_FOOD_SUBTOTAL
+        self.SSM_TOTAL = SSM_TOTAL
 
 class Clothes(db.Model):
     __tablename__ = 'clothes'
@@ -48,7 +168,6 @@ class PhaseNewCoronic(db.Model):
         self.phase = phase
         self.new_coronic = new_coronic
 
-
 class NaverComprehensiveData(db.Model):
 
     __tablename__ = 'naver_comprehensive_data'
@@ -86,33 +205,8 @@ class NaverComprehensiveData(db.Model):
         self.fashion_accessories = fashion_accessories
         self.beauty = beauty
 
-
-class Movie(db.Model):
-
-    __tablename__ = 'movie'
-
-    id = db.Column(db.Integer, primary_key= True, nullable= False)
-    date_day = db.Column(db.DateTime, nullable= False)
-    korea_sales = db.Column(db.Integer, nullable= False)
-    korea_audience_num = db.Column(db.Integer, nullable=False)
-    foreign_audience_num = db.Column(db.Integer, nullable=False)
-    foreign_sales = db.Column(db.Integer, nullable=False)
-    total_sales = db.Column(db.Integer, nullable=False)
-    total_audience = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, date_day, korea_sales, korea_audience_num, foreign_audience_num,foreign_sales,
-                 total_sales, total_audience):
-        self.date_day = date_day
-        self.korea_sales = korea_sales
-        self.korea_audience_num = korea_audience_num
-        self.foreign_audience_num = foreign_audience_num
-        self.foreign_sales = foreign_sales
-        self.total_sales = total_sales
-        self.total_audience = total_audience
-
-
-class Metro(db.Model):
-    __tablename__ = 'metro'
+class SeoulBus(db.Model):
+    __tablename__ = 'seoulbus'
     id = db.Column(db.Integer, primary_key=True)
     date_day = db.Column(db.DateTime, nullable=False)
     get_on = db.Column(db.Integer, nullable=False)
@@ -122,3 +216,38 @@ class Metro(db.Model):
         self.date_day = date_day
         self.get_on = get_on
         self.get_off = get_off
+
+class MetroSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'date_day', 'get_on', 'get_off')
+
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'date_day', 'korea_sales', 'korea_audience_num', 'foreign_audience_num','foreign_sales',
+                 'total_sales', 'total_audience')
+
+class ConsumptionSchema(ma.Schema):
+    class Meta:
+        fields = ('ID', 'DATE', 'MARKET_NONFOOD_HOMEWARE_CULTURE', 'MARKET_NONFOOD_CLOTHING', 'MARKET_NONFOOD_DAILY', 'MARKET_NONFOOD_SPORT', 'MARKET_NONFOOD_CHANDLERY', 'MARKET_NONFOOD_SUBTOTAL',
+                'MARKET_FOOD', 'MARKET_TOTAL', 'DEPARTMENT_NONFOOD_CHANDLERY', 'DEPARTMENT_NONFOOD_WOMENSUITS', 'DEPARTMENT_NONFOOD_WOMENCASUAL', 'DEPARTMENT_NONFOOD_MENCLOTHING', 'DEPARTMENT_NONFOOD_KIDS_SPORT',
+                'DEPARTMENT_NONFOOD_HOMEWARE', 'DEPARTMENT_NONFOOD_LUXURY', 'DEPARTMENT_NONFOOD_SUBTOTAL', 'DEPARTMENT_FOOD', 'DEPARTMENT_TOTAL', 'CONVENIENCE_NONFOOD_DAILY', 'CONVENIENCE_NONFOOD_CHANDLERY',
+                'CONVENIENCE_NONFOOD_CIGARETTE_ETC','CONVENIENCE_NONFOOD_SUBTOTAL', 'CONVENIENCE_FOOD_BEVERAGE_PROCESSED', 'CONVENIENCE_FOOD_INSTANT_FRESH', 'CONVENIENCE_FOOD_SUBTOTAL', 'CONVENIENCE_TOTAL',
+                'SSM_NONFOOD_DAILY', 'SSM_NONFOOD_CHANDLERY', 'SSM_NONFOOD_SUBTOTAL', 'SSM_FOOD_AGRICULTURAL_MARINE_LIVESTOCK', 'SSM_FOOD_FRESH', 'SSM_FOOD_PROCESSED', 'SSM_FOOD_SUBTOTAL', 'SSM_TOTAL')
+
+class ClothesSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'date_time', 'total')
+
+class ConcertSchema(ma.Schema):
+    class Meta:
+        fields = ('id','date_mon', 'perform_num', 'opening_num','showing_num','sales','book_num')
+
+class PhaseNewCoronicSchema(ma.Schema):
+    class Meta:
+        fields = ('id','date_day', 'phase','new_coronic')
+
+class NaverComprehensiveDataSchema(ma.Schema):
+    class Meta:
+        fields = ('id','date_day', 'furniture', 'book', 'home_appliance', 'duty_free', 'living_health', 'sport_leisure',
+                 'food', 'amenity', 'childbirth', 'fashion_clothes', 'fashion_accessories', 'beauty')
+
